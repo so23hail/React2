418460c4a14158a6fbd0dae53cbc2bad214c0c8c
so23hail <sohail23khan@gmail.com>
2025-01-12 07:27:53 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,5 +1,48 @@
-const heading=React.createElement("h1",
-    {id:"heading"},
-    "Hello World from React")
-        const root=ReactDOM.createRoot(document.getElementById("root"))
-        root.render(heading)
+import React from "react"
+import ReactDOM, { createRoot } from "react-dom/client"
+
+const heading= (
+    <h1 id="heading">
+    This is  simple headind created in the functional component
+    </h1>
+    )
+
+
+const Jsxheading = () => ( <h1 id="heading">
+    This is created using JSX
+    </h1>
+    )
+
+
+    const Header= () =>{
+        return(
+            <div className="Header">
+                <div>
+                    
+                </div>
+            </div>
+
+        )
+    }
+
+    const AppLayout = () =>{
+        return(
+            <div className="App">
+            
+            </div>
+        )
+    }
+
+const HeadingComponent=()=>(
+    <div id="container">
+        <Jsxheading />
+        {heading}
+        <h1 className="heading"> This is heading in functional component</h1> 
+
+    </div>
+)    
+
+const root=createRoot(document.getElementById("root"))
+root.render(<HeadingComponent />)
+
+
